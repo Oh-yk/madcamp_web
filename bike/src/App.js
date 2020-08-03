7075a578e074cdceb0f98c25978f79e6938ffedf
YookyungOh <gamtangerine@gmail.com>
2020-08-03 16:14:50 +0900

    main page

--- bike/src/App.js
+++ bike/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+          <Layout>
+              <Header className="header-color" title="Title" scroll>
+                  <Navigation>
+                      <Link to="/map">Map</Link>
+                      <Link to="/equipment">Equipment</Link>
+                      <Link to="/tips">Tips</Link>
+                      <Link to="/mypage">My Page</Link>
+                  </Navigation>
+              </Header>
+              <Drawer title="Title">
+                  <Navigation>
+                  <Link to="/map">Map</Link>
+                      <Link to="/equipment">Equipment</Link>
+                      <Link to="/tips">Tips</Link>
+                      <Link to="/mypage">My Page</Link>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                  <div className="page-content" />
+                  <Main/>
+              </Content>
+          </Layout>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ bike/src/components/equipment.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Equipment extends Component {
+    render() {
+        return (
+            <div><h1>Equipment Page</h1></div>
+        )
+    }
+}
+
+export default Equipment;
