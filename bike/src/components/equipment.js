149c0d4c611670f2f5d004ed72a39ed8b4dee7e1
YookyungOh <gamtangerine@gmail.com>
2020-08-03 21:27:06 +0900

    equipment - add search

--- bike/src/components/equipment.js
+++ bike/src/components/equipment.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton, Textfield } from 'react-mdl';
 
 class Equipment extends Component {
     constructor(props) {
@@ -10,53 +10,62 @@ class Equipment extends Component {
     toggleCategories() {
         if(this.state.activeTab === 0) {
             return (
-                <div className="equipment-grid">
-                    <Card shadow={3} style={{minWidth: "450", margin: 'auto'}}>
-                        <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://img.hankyung.com/photo/cts/201801/a40bb51cda4d84c5d28e84d355867694.jpg) center / cover'}} >HJC480 Helmet</CardTitle>
-                        <CardText>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        </CardText>
-                        <CardActions border>
-                            <Button colored>Buy</Button>
-                            <Button colored>Browse</Button>
-                            <Button colored>Like</Button>
-                        </CardActions>
-                        <CardMenu style={{color: '#fff'}}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
+                <div>
+                    <div>
+                        <Textfield
+                            onChange={() => {}}
+                            label="Expandable Input"
+                            expandable
+                            expandableIcon="search"
+                        />
+                    </div>
+                    <div className="equipment-grid">
+                        <Card shadow={3} style={{minWidth: "450", margin: 'auto'}}>
+                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://img.hankyung.com/photo/cts/201801/a40bb51cda4d84c5d28e84d355867694.jpg) center / cover'}} >HJC480 Helmet</CardTitle>
+                            <CardText>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>Buy</Button>
+                                <Button colored>Browse</Button>
+                                <Button colored>Like</Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
 
-                    <Card shadow={3} style={{minWidth: "450", margin: 'auto'}}>
-                        <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://img.hankyung.com/photo/cts/201801/a40bb51cda4d84c5d28e84d355867694.jpg) center / cover'}} >HJC480 Helmet</CardTitle>
-                        <CardText>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        </CardText>
-                        <CardActions border>
-                            <Button colored>Buy</Button>
-                            <Button colored>Browse</Button>
-                            <Button colored>Like</Button>
-                        </CardActions>
-                        <CardMenu style={{color: '#fff'}}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
+                        <Card shadow={3} style={{minWidth: "450", margin: 'auto'}}>
+                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://img.hankyung.com/photo/cts/201801/a40bb51cda4d84c5d28e84d355867694.jpg) center / cover'}} >HJC480 Helmet</CardTitle>
+                            <CardText>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>Buy</Button>
+                                <Button colored>Browse</Button>
+                                <Button colored>Like</Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
 
-                    <Card shadow={3} style={{minWidth: "450", margin: 'auto'}}>
-                        <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://img.hankyung.com/photo/cts/201801/a40bb51cda4d84c5d28e84d355867694.jpg) center / cover'}} >HJC480 Helmet</CardTitle>
-                        <CardText>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        </CardText>
-                        <CardActions border>
-                            <Button colored>Buy</Button>
-                            <Button colored>Browse</Button>
-                            <Button colored>Like</Button>
-                        </CardActions>
-                        <CardMenu style={{color: '#fff'}}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
+                        <Card shadow={3} style={{minWidth: "450", margin: 'auto'}}>
+                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://img.hankyung.com/photo/cts/201801/a40bb51cda4d84c5d28e84d355867694.jpg) center / cover'}} >HJC480 Helmet</CardTitle>
+                            <CardText>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>Buy</Button>
+                                <Button colored>Browse</Button>
+                                <Button colored>Like</Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                    </div>
                 </div>
-        
             )
         } else if(this.state.activeTab === 1) {
             return (
@@ -74,6 +83,10 @@ class Equipment extends Component {
             return (
                 <div><h1>Bags Recommendation List</h1></div>
             )
+        } else if(this.state.activeTab === 5) {
+            return (
+                <div><h1>ADD MORE</h1></div>
+            )
         }
     }
 
@@ -86,6 +99,7 @@ class Equipment extends Component {
                     <Tab>Clothes</Tab>
                     <Tab>Bicycle Tools</Tab>
                     <Tab>Bags</Tab>
+                    <Tab>More</Tab>
                 </Tabs>
 
                 <section className="equipment-grid">
