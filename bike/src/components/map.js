62b2509c72a4eed400170d01f4fe4417a37fee5e
KimGyeongyeon <kgy556@kaist.ac.kr>
2020-08-04 15:22:44 +0900

    labtop

---
+++ bike/src/components/map.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+    render() {
+        return (
+            <div><h1>Map Page</h1></div>
+        )
+    }
+}
+
+export default Map;
